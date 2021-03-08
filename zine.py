4bf960561dd38bea43f47d05b13c7102f72422f4
Matt 3 <not.mattowen@gmail.com>
2021-03-07 19:44:11 -0500

    :fish_cake: Convert into a triple-loop

--- zine.py
+++ zine.py
@@ -4,16 +4,18 @@
 import argparse
 import io
 
-from PIL import Image, ImageDraw
 from collections import namedtuple
 
 import fitz
+
+from PIL import Image, ImageDraw
 
 
 WIDTH, HEIGHT = fitz.PaperSize("A4")
 
 
 def page_image(zine):
+    """Return a PIL.Image representing that page"""
     num = zine.number
     w = HEIGHT//4 * 1
     h = WIDTH//2 * 1
@@ -65,65 +67,34 @@ if __name__ == "__main__":
 
     with fitz.open() as doc:
 
-        front = doc.newPage(width=WIDTH, height=HEIGHT)
+        for s in range(2):
 
-        # First split
-        front.draw_line((WIDTH/2, 0), (WIDTH/2, HEIGHT))
+            page = doc.newPage(width=WIDTH, height=HEIGHT)
 
-        # Across
-        # front.draw_line((0, 1*HEIGHT/4), (WIDTH, 1*HEIGHT/4))
-        # front.draw_line((0, 2*HEIGHT/4), (WIDTH, 2*HEIGHT/4))
-        # front.draw_line((0, 3*HEIGHT/4), (WIDTH, 3*HEIGHT/4))
+            for j in range(4):
+                for i in range(2):
+                    x = i*WIDTH/2 + WIDTH/4
+                    y = j*HEIGHT/4 + HEIGHT/8
 
-        for j in range(4):
-            for i in range(2):
-                x = i*WIDTH/2 + WIDTH/4
-                y = j*HEIGHT/4 + HEIGHT/8
+                    zine = PAGES.get(8*s + 2*j+i)
 
-                zine = PAGES.get(2*j+i)
+                    if zine is None:
+                        raise "This failed in a way that we did not expect."
 
-                # ...
-                img = page_image(zine)
-                byte_arr = io.BytesIO()
-                img.save(byte_arr, format="JPEG")
+                    # ...
+                    img = page_image(zine)
+                    byte_arr = io.BytesIO()
+                    img.save(byte_arr, format="JPEG")
 
-                rect = fitz.Rect(
-                    i * WIDTH/2.0,
-                    j * HEIGHT/4.0,
+                    rect = fitz.Rect(
+                        i * WIDTH/2.0,
+                        j * HEIGHT/4.0,
 
-                    i * WIDTH/2.0 + WIDTH/2.0,
-                    j * HEIGHT/4.0 + HEIGHT/4,
-                )
+                        i * WIDTH/2.0 + WIDTH/2.0,
+                        j * HEIGHT/4.0 + HEIGHT/4,
+                    )
 
-                front.insert_image(rect, stream=byte_arr, rotate=zine.rotation)
-                front.draw_rect(rect)
-
-        back = doc.newPage(width=WIDTH, height=HEIGHT)
-
-        for j in range(4):
-            for i in range(2):
-                x = i*WIDTH/2 + WIDTH/4
-                y = j*HEIGHT/4 + HEIGHT/8
-
-                zine = PAGES.get(8 + 2*j+i)
-
-                if zine is None:
-                    raise "This failed in a way that we did not expect."
-
-                # ...
-                img = page_image(zine)
-                byte_arr = io.BytesIO()
-                img.save(byte_arr, format="JPEG")
-
-                rect = fitz.Rect(
-                    i * WIDTH/2.0,
-                    j * HEIGHT/4.0,
-
-                    i * WIDTH/2.0 + WIDTH/2.0,
-                    j * HEIGHT/4.0 + HEIGHT/4,
-                )
-
-                back.insert_image(rect, stream=byte_arr, rotate=zine.rotation)
-                back.draw_rect(rect)
+                    page.insert_image(rect, stream=byte_arr, rotate=zine.rotation)
+                    page.draw_rect(rect)
 
         doc.save("sample.pdf")
