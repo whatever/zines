097ce031396440cb10c03240bf8fc6877afacd3f
Matt 3 <not.mattowen@gmail.com>
2021-03-09 00:23:48 -0500

    default/pdf/not-found simple pages

--- server.py
+++ server.py
@@ -7,25 +7,33 @@ from zine import PAGES, generate_pdf_doc
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-
         if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(b"yea2")
+            self._do_get_default()
         elif self.path == "/pdf":
-            self._do_get_PDF()
+            self._do_get_pdf()
+        else:
+            self._do_get_not_found()
+    
+    def _do_get_default(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"yea2")
+    
+    def _do_get_not_found(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"not found")
 
-    def _do_get_PDF(self):
+    def _do_get_pdf(self):
         self.send_response(200)
         self.send_header("Content-type", "application/x-pdf")
         self.end_headers()
-
         doc = generate_pdf_doc([
             PAGES.get(i)
             for i in range(16)
         ])
-
         self.wfile.write(doc.tobytes())
 
 
